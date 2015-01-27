8c70f53469e378596f72994ee543d95c6e3ba50a
pratik12 <p_bidkar@encs.concordia.ca>
2015-01-26 19:27:51 -0500

    Changes to Player class

--- discWorld_Build1_Team15/src/com/app/Player.java
+++ discWorld_Build1_Team15/src/com/app/Player.java
@@ -1,19 +1,41 @@
 package com.app;
-
+/**
+ * Player class holding properties of a player
+ * @author Pratik
+ *
+ */
 public class Player {
 	
 	private String minion_Color;
 	private int minion_Quantity;
+	public int number_of_buildings = 6;
+	private String winning_condition;
+	public String random_event_card;
+	private double player_amount;
 	
+	/**
+	 * Constructor to set the properties of player
+	 * @param minion_Color
+	 * @param quantity
+	 */
 	public Player(String minion_Color, int quantity){
 		set_Minion_Color(minion_Color);
 		set_Minion_Quantity(quantity);
+		set_Player_amount(10);
 	}
 	
+	/**
+	 * 
+	 * @return color of minion
+	 */
 	public String get_Minion_Color() {
 		return minion_Color;
 	}
 	
+	/**
+	 * 
+	 * @param minion_Color the color of minion to be set
+	 */
 	public void set_Minion_Color(String minion_Color) {
 		this.minion_Color = minion_Color;
 	}
@@ -25,7 +47,36 @@ public class Player {
 	public void set_Minion_Quantity(int minion_Quantity) {
 		this.minion_Quantity = minion_Quantity;
 	}
-	
+
+	/**
+	 * @return the winning_condition
+	 */
+	public String get_Winning_condition() {
+		return winning_condition;
+	}
+
+	/**
+	 * @param winning_condition the winning_condition to set
+	 */
+	public void set_Winning_condition(String winning_condition) {
+		this.winning_condition = winning_condition;
+	}
+
+	/**
+	 * @return the player_amount
+	 */
+	public double get_Player_amount() {
+		return player_amount;
+	}
+
+	/**
+	 * @param player_amount the player_amount to set
+	 */
+	public void set_Player_amount(double player_amount) {
+		this.player_amount = player_amount;
+	}
+
+		
 	
 
 }
