8e8b8871efa973142971526cd39af9f850c00f91
salameh Abdollahi Sohi <salameh.abdollahi@gmail.com>
2015-02-03 18:59:51 -0500

    testcase

---
+++ discWorld_Build1_Team15/src/com/testcase/TestSuite.java
@@ -0,0 +1,18 @@
+package com.testcase;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mahdiye
+ * Date: 2/1/15
+ * Time: 9:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BoardGameClassTest.class, FileManagerClassTest.class, DiscWorldClassTest.class})
+public class TestSuite {
+
+}
