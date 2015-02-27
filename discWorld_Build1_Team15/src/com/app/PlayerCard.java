87298176b9e6f96b4ded38aa87c7b7f4a9f9f60c
pratik12 <p_bidkar@encs.concordia.ca>
2015-02-26 21:02:41 -0500

    updated player card

--- discWorld_Build1_Team15/src/com/app/PlayerCard.java
+++ discWorld_Build1_Team15/src/com/app/PlayerCard.java
@@ -1,13 +1,68 @@
 package com.app;
 
+import java.io.ObjectInputStream.GetField;
+
 /**
  * The Class PlayerCard.
  *
  * @author Pratik
  * Class of player cards that will be given out to players when the game starts
  */
-public class PlayerCard {
+public enum PlayerCard implements Actions{
+	
 
+	BOGGIS("BOGGIS","green","DeckPile"),
+	
+	BEGGARSGUILD("BEGGARSGUILD","green","DeckPile"),
+	
+	AMBANK("AMBANK","green","DeckPile"),
+	AMSANCTUARY("AMSANCTUARY","green","DeckPile"),
+	ANGUA("ANGUA","green","DeckPile"),
+	AGONYAUNTS("AGONYAUNTS","green","DeckPile"),
+	DYSK("DYSK","green","DeckPile"),
+	DUCKMAN("DUCKMAN","green","DeckPile"),
+	DRUMKNOTT("DRUMKNOTT","green","DeckPile"),
+	CMOTDIBBLER("CMOTDIBBLER","green","DeckPile"),
+	DRCRUCES("DRCRUCES","green","DeckPile"),
+	CAPTAINCARROT("CAPTAINCARROT","green","DeckPile"),
+	MRSCAKE("MRSCAKE","green","DeckPile"),
+	GROAT("GROAT","green","DeckPile"),
+	GASPODE("GASPODE","green","DeckPile"),
+	FRESHSTARTCLUB("GROAT","green","DeckPile"),
+	FOULOLERON("GROAT","green","DeckPile"),
+	THEFOOLSGUILD("GROAT","green","DeckPile"),
+	THEFIREBRIGADE("GROAT","green","DeckPile"),
+	INIGOSKIMMER("GROAT","green","DeckPile"),
+	HISTORYMONKS("GROAT","green","DeckPile"),
+	HEX("GROAT","green","DeckPile"),
+	HERENOW("GROAT","green","DeckPile"),
+	HARRYKING("GROAT","green","DeckPile"),
+	GRYLE("GROAT","green","DeckPile"),
+	HARGAHOUSEOFRIBS("GROAT","green","DeckPile"),
+	THEPEELEDNUTS("THEPEELEDNUTS","green","DeckPile"),
+	THEOPERAHOUSE("THEOPERAHOUSEOPERAHOUSE","green","DeckPile"),
+	NOBBYNOBBSS("NOBBYNOBBSS","green","DeckPile"),
+	MODO("MODO","green","DeckPile"),
+	THEMENDEDDRUM("THEMENDEDDRUM","green","DeckPile"),
+	LIBRARIAN("LIBRARIAN","green","DeckPile"),
+	LEONARDOFQUIRM("LEONARDOFQUIRM","green","DeckPile"),
+	SHONKYSHOP("SHONKYSHOP","green","DeckPile"),
+	SACHARISSACRIPSLOCK("SACHARISSACRIPSLOCK","green","DeckPile"),
+	ROSIEPALM("ROSIEPALM","green","DeckPile"),
+	RINCEWIND("RINCEWIND","green","DeckPile"),
+	THEROYALMINT("THEROYALMINT","green","DeckPile"),
+	QUEENMOLLY("QUEENMOLLY","green","DeckPile"),
+	PINKPUSSYCATCLUB("PINKPUSSYCATCLUB","green","DeckPile"),
+	ZORGOTHERETROPHRENOLOGIST("ZORGOTHERETROPHRENOLOGIST","green","DeckPile"),
+	DRWHITEFACE("DRWHITEFACE","green","DeckPile"),
+	WALLACESONKY("WALLACESONKY","green","DeckPile"),
+	THESEAMSTRESSESGUILD("THESEAMSTRESSESGUILD","green","DeckPile"),
+	MRPINANDMRTULIP("MRPINANDMRTULIP","green","DeckPile"),
+	THETHIEVESGUILD("THETHIEVESGUILD","green","DeckPile");
+		
+	BoardGame bg;
+	private static String location ;
+	
 	/** The color. */
 	private String color;
 
@@ -15,10 +70,8 @@ public class PlayerCard {
 	private String name;
 
 	/** The location. */
-	private String location;
 
 	/** The id of playing card **/
-	private int number;
 	/**
 	 * Instantiates a new player card.
 	 *
@@ -26,15 +79,14 @@ public class PlayerCard {
 	 * @param name the name
 	 * @param location the location
 	 */
-	public PlayerCard(int number, String color, String name, String location){
+	private PlayerCard( String name, String color, String location){
 
 		setColor(color);
 		setLocation(location); 
 		setName(name);
-		setNumber(number);
 
 	}
-
+	
 	/**
 	 * Gets the color.
 	 *
@@ -89,20 +141,38 @@ public class PlayerCard {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	/**
-	 * @return the number
-	 */
-	public int getNumber() {
-		return number;
+	@Override
+	public boolean addMoney(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+		
 	}
-
-	/**
-	 * @param number the number to set
-	 */
-	public void setNumber(int number) {
-		this.number = number;
+	@Override
+	public boolean moveMinion() {
+		// TODO Auto-generated method stub
+		return false;
 	}
-
+	@Override
+	public boolean assasinate(String pieceToRemove) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean scroll() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean playAnotherCard() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean interrupt() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 
 }
