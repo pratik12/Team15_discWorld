58ae00a7cafad757323072dd2c713d9e2f2e5588
pratik12 <p_bidkar@encs.concordia.ca>
2015-02-26 21:00:13 -0500

    added player cards to teh game

---
+++ discWorld_Build1_Team15/src/com/app/Actions.java
@@ -0,0 +1,12 @@
+package com.app;
+
+public interface Actions {
+
+	public boolean addMoney(int amount);
+	public boolean moveMinion();
+	public boolean assasinate(String pieceToRemove);
+	public boolean scroll();
+	public boolean playAnotherCard();
+	public boolean interrupt();
+	
+}
---
+++ discWorld_Build1_Team15/src/com/app/StartGame.java
@@ -0,0 +1,10 @@
+package com.app;
+
+public class StartGame {
+
+	
+	public void startGame(){
+		
+	}
+
+}
