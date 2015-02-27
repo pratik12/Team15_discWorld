95e4845b6634db6ef933b4a0c3cb3407315d8e2d
maahzaade <mahdiye.nikraftar@gmail.com>
2015-02-26 20:17:26 -0500

    implementing factory method

---
+++ discWorld_Build1_Team15/src/com/app/rules/Chrysoprase.java
@@ -0,0 +1,34 @@
+package com.app.rules;
+
+import com.app.Area;
+import com.app.BoardGame;
+import com.app.Player;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mahdiye
+ * Date: 2/24/15
+ * Time: 3:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Chrysoprase implements WinningCircumstances {
+
+    @Override
+    public Boolean isWinner() {
+        Player currentPlayer = null;
+        int totalProperty = 0;
+        for (Player player : BoardGame.playersInGame) {
+            if (player.getWinningCondition().equals("")) {
+                currentPlayer = player;
+            }
+        }
+        if (currentPlayer != null && currentPlayer.getPlayerAreas() != null && !currentPlayer.getPlayerAreas().isEmpty()) {
+            for (Area area : currentPlayer.getPlayerAreas()) {
+                totalProperty += area.getCostOfArea();
+            }
+            totalProperty += currentPlayer.getPlayerAmount();
+            totalProperty -= 12 * currentPlayer.getPlayerLoan();
+        }
+        return totalProperty >= 50;
+    }
+}
