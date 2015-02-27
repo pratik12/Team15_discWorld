f54ecf6dd1653fd62ebc1ba3a796fe96defcf4cf
maahzaade <mahdiye.nikraftar@gmail.com>
2015-02-26 20:20:26 -0500

    implementing factory method pattern

---
+++ discWorld_Build1_Team15/src/com/app/rules/WinningCircumstances.java
@@ -0,0 +1,14 @@
+package com.app.rules;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mahdiye
+ * Date: 2/24/15
+ * Time: 3:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface WinningCircumstances {
+
+    public abstract  Boolean isWinner();
+
+}
