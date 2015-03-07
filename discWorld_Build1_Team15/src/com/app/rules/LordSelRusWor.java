8c0d7121d62fe5073f07a0333954f1b1bf1f3119
maahzaade <mahdiye.nikraftar@gmail.com>
2015-03-07 13:06:58 -0500

    fix to do

--- discWorld_Build1_Team15/src/com/app/rules/LordSelRusWor.java
+++ discWorld_Build1_Team15/src/com/app/rules/LordSelRusWor.java
@@ -1,7 +1,10 @@
 package com.app.rules;
 
+import com.app.Area;
 import com.app.BoardGame;
 import com.app.Player;
+
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -22,17 +25,48 @@ public class LordSelRusWor implements WinningCircumstances {
             }
         }
         if (currentPlayer != null && currentPlayer.getPlayerAreas() != null && !currentPlayer.getPlayerAreas().isEmpty()) {
-//            if (numberOfPlayers == 2 && currentPlayer.getPlayerAreas().size() == 7) {
-//            }
-//            else if (numberOfPlayers == 3 && currentPlayer.getPlayerAreas().size() == 5){
-//            }
-//            else if (numberOfPlayers == 4 && currentPlayer.getPlayerAreas().size() == 4){
-//            }
-//            else
-           // return Boolean.FALSE;
-        //} else
-        // return Boolean.FALSE;
-    }
+            if (numberOfPlayers == 2) {
+                checkControlledAreas(currentPlayer, 7);
+            } else if (numberOfPlayers == 3) {
+                checkControlledAreas(currentPlayer, 5);
+            } else if (numberOfPlayers == 4) {
+                checkControlledAreas(currentPlayer, 4);
+            }
+        }
         return true;
+    }
+
+    private Boolean checkControlledAreas(Player currentPlayer, int numberOfAreaTocheck) {
+        int currentTotalProperty = 0;
+        int totalPieces = 0;
+        int controlledAreCounter = 0;
+        int countedCurrentArealength = 0;
+
+        //todo: page 8 from 8 Demons from the Dungeon Dimensions
+        if (Boolean.TRUE) {
+            return Boolean.FALSE;
+
+        } else {
+            while (controlledAreCounter < numberOfAreaTocheck && countedCurrentArealength <= currentPlayer.getPlayerAreas().size()) {
+
+                ArrayList<Area> areas = currentPlayer.getPlayerAreas();
+                for (Player p : areas.get(countedCurrentArealength).getPlayersInThisAreas()) {
+
+                    if (!(p.getPlayerColor().equalsIgnoreCase(currentPlayer.getPlayerColor()))) {
+                        currentTotalProperty += p.getNumberOfBuildings() + p.getMinionQuantity();
+                        totalPieces += currentPlayer.getNumberOfBuildings() + currentPlayer.getMinionQuantity();
+                    } else {
+                        controlledAreCounter++;
+                    }
+
+                    if (totalPieces > currentTotalProperty)
+                        controlledAreCounter++;
+                    countedCurrentArealength++;
+                }
+
+                return controlledAreCounter >= numberOfAreaTocheck;
+            }
+        }
+        return Boolean.TRUE;
     }
 }
