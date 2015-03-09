f3d2e6e5ec74ac3825cb445760c50b5883d247fa
pratik12 <p_bidkar@encs.concordia.ca>
2015-03-09 18:47:07 -0400

    changes

---
+++ discWorld_Build1_Team15/src/com/app/PlayerCardUtility.java
@@ -0,0 +1,37 @@
+package com.app;
+
+import com.app.PlayingCardSystem.GreenPlayerCardEnum;
+
+public abstract class PlayerCardUtility {
+	
+	
+	
+	public static void displayPlayerCardDetails(Player player){
+		
+		
+		System.out.println("*******AVAILABLE PLAYING CARD INFORMATION**************");
+		for(GreenPlayerCardEnum gc : player.getPlayersPlayingCard()){
+			System.out.print("PLAYING CARD NAME: ");
+			System.out.printf("%-30s\n",gc.getName());
+			System.out.print("ACTIONS TO PERFORM: ");
+			for(String str : gc.getSymbols()){
+				System.out.printf("%-20s ", str );
+				}
+			System.out.println();
+		}
+		
+	}
+
+	
+	public static GreenPlayerCardEnum getEnumInstance(String enumName){
+		GreenPlayerCardEnum temp = null;
+		for(GreenPlayerCardEnum gc : BoardGame.player_cards){
+			if(enumName.trim().equalsIgnoreCase(gc.name())){
+				temp  = gc;
+				break;
+			}
+			break;
+		}
+		return temp;
+	}
+}
--- discWorld_Build1_Team15/src/com/app/StartPlayingGame.java
+++ discWorld_Build1_Team15/src/com/app/StartPlayingGame.java
@@ -1,27 +1,66 @@
 package com.app;
 
+import java.util.Scanner;
+
+import org.json.JSONException;
+
+import com.app.PlayingCardSystem.GreenPlayerCardEnum;
 import com.app.rules.WinningCircumstancesFactory;
 
 public class StartPlayingGame {
 	
 	public int currentPlayer = 0;
 	//WinningCircumstancesFactory factoryObj = new WinningCircumstancesFactory();
-	
-	public void start(){
+	Scanner in = new Scanner(System.in);
+	public void start() throws JSONException{
 		
-		for(int i=0; i<BoardGame.playersInGame.size();i++ ){
+		
+		if(BoardGame.player_cards.size()!=0 || !(checkWinningConditionEveryPlayer()) ){
 			
-		if(WinningCircumstancesFactory.getWinningCircumstance(BoardGame.playersInGame.get(i).getWinningCondition()).isWinner()){
-			System.out.println("Player playing with"+BoardGame.playersInGame.get(i).getPlayerColor()+" has won");
+			// start playing cards
+			// at the end of his turn pass turn to another player
+			for(int i=0; i<BoardGame.playersInGame.size();i++ ){
+				System.out.println("Player "+BoardGame.playersInGame.get(i).getPlayerColor()+
+						". Its is your turn to play");
+				//displayPersonalityCard(BoardGame.playersInGame.get(i));
+				
+				displayPlayerInstructions(BoardGame.playersInGame.get(i));
+				String res = in.nextLine();
+				PlayerCardUtility.getEnumInstance(res).performTasks(BoardGame.playersInGame.get(i));
+		}
+			
+		}
+		else{
+		//	System.out.println("Player playing with"+BoardGame.playersInGame.get(i).getPlayerColor()+" has won");
 			System.out.println("Hope you all enjoyed!!!!!!!!!!!!!!!");
 			System.exit(0);
 		}
-		else{
-			// start playing cards
-			// at the end of his turn pass turn to another player
-		}
 		
+		
+	}
+	
+	private void displayPersonalityCard(Player player) {
+		
+		System.out.println("Your personality card is : " +player.getWinningCondition());
+		for(GreenPlayerCardEnum gc : player.getPlayersPlayingCard()){
+			
 		}
+	}
+
+	private void displayPlayerInstructions(Player player) {
+		
+		PlayerCardUtility.displayPlayerCardDetails(player);
+	}
+
+	public boolean checkWinningConditionEveryPlayer(){
+		
+		for(int i=0; i<BoardGame.playersInGame.size();i++ ){
+		
+			if(WinningCircumstancesFactory.getWinningCircumstance(BoardGame.playersInGame.get(i).getWinningCondition()).isWinner())
+				return true;
+			
+		}
+		return false;
 	}
 	
 	
