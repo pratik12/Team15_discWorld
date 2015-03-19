cf8d9225d55737f06c640dd8971dc5986da199fd
pratik12 <p_bidkar@encs.concordia.ca>
2015-03-19 16:32:22 -0400

    updated changes to playercardutility class

--- discWorld_Build1_Team15/src/com/app/PlayerCardUtility.java
+++ discWorld_Build1_Team15/src/com/app/PlayerCardUtility.java
@@ -14,16 +14,20 @@ public abstract class PlayerCardUtility {
 	public static void displayPlayerCardDetails(Player player){
 		
 		
-		System.out.println("*******AVAILABLE PLAYING CARD INFORMATION**************");
-		for(GreenPlayerCardEnum gc : player.getPlayersPlayingCard()){
-			System.out.print("PLAYING CARD NAME: ");
-			System.out.printf("%-30s",gc.getName());
-			System.out.print("ACTIONS TO PERFORM: ");
-			for(String str : gc.getSymbols()){
-				System.out.printf("%-20s", str );
-				}
-			System.out.println();
-		}
+		try{
+			System.out.println("*******AVAILABLE PLAYING CARD INFORMATION**************");
+			for(GreenPlayerCardEnum gc : player.getPlayersPlayingCard()){
+				System.out.print("PLAYING CARD NAME: ");
+				System.out.printf("%-30s",gc.getName());
+				System.out.print("ACTIONS TO PERFORM: ");
+				for(String str : gc.getSymbols()){
+					System.out.printf("%-20s\n", str );
+					}
+				System.out.println();
+			}
+			}catch(NullPointerException e){
+				throw e;
+			}
 		
 	}
 
