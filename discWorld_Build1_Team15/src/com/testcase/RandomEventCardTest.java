b95883335f8dd37aa962fbf3ae314d6d6e2eea22
maahzaade <mahdiye.nikraftar@gmail.com>
2015-04-01 10:56:27 -0400

    test cases are created

--- discWorld_Build1_Team15/src/com/testcase/RandomEventCardTest.java
+++ discWorld_Build1_Team15/src/com/testcase/RandomEventCardTest.java
@@ -1,9 +1,17 @@
 package com.testcase;
 
+import com.app.BoardGame;
+import com.app.FileManager;
+import com.app.Player;
+import com.app.PlayingCardSystem.GreenPlayerCardEnum;
 import com.app.rules.RandomEventCard;
+import org.json.JSONException;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+
 import static org.junit.Assert.assertTrue;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,11 +20,44 @@ import static org.junit.Assert.assertTrue;
  * Time: 9:04 PM
  * To change this template use File | Settings | File Templates.
  */
+
 public class RandomEventCardTest {
 
     @Test
+    public void testTheDragon() {
+        try {
+            FileManager.loadFile("theDragon.txt");
+        } catch (JSONException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+
+        Player currentPlayer = BoardGame.playersInGame.get(3);
+        try {
+            assertTrue(RandomEventCard.GLOBALOBJ.doTheTasks(currentPlayer, RandomEventCard.TheDragon, GreenPlayerCardEnum.RINCEWIND));
+        } catch (JSONException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+     /*
+    @Test
     public void testFlood() {
-        assertTrue(RandomEventCard.GLOBALOBJ.doTheTasks(null, RandomEventCard.Flood, null));
+        try {
+            FileManager.loadFile("C:\\fakeFile.txt");
+        } catch (JSONException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        Player currentPlayer = new Player("");
+        try {
+            assertTrue(RandomEventCard.GLOBALOBJ.doTheTasks(currentPlayer, RandomEventCard.Flood, GreenPlayerCardEnum.RINCEWIND));
+        } catch (JSONException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
     @Test
@@ -54,5 +95,6 @@ public class RandomEventCardTest {
     public void testEarthquake() {
         assertTrue(RandomEventCard.GLOBALOBJ.doTheTasks(null, RandomEventCard.Earthquake, null));
     }
+    */
 
 }
